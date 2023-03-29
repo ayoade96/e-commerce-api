1645ec30313cbff174a695446a8ac340281b33f4
Ayoade Abdulrahman <abdulrahmanadesile@yahoo.com>
2023-03-29 11:08:37 +0100

    e-commerce-api

---
+++ app.js
@@ -0,0 +1,87 @@
+// Express
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+require('express-async-errors');
+
+const app = express();
+dotenv.config();
+const morgan = require('morgan')
+const cookieParser = require('cookie-parser')
+const fileUpload = require('express-fileupload')
+
+const rateLimiter = require('express-rate-limit');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const mongoSanitize = require('express-mongo-sanitize');
+
+
+//Database
+mongoose.set('strictQuery', true)
+
+//routers
+const authRouter = require('./routes/authRoutes')
+const userRouter = require('./routes/userRoutes')
+const productRouter = require('./routes/productRoutes')
+const reviewRouter = require('./routes/reviewRoutes')
+const orderRouter = require('./routes/orderRoutes')
+
+//middleware
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 60,
+  })
+);
+
+app.use(helmet());
+app.use(xss());
+app.use(mongoSanitize());
+
+
+app.use(morgan('tiny'))
+app.use(express.json())
+app.use(cookieParser(process.env.JWT_SECRET));
+
+app.use(express.static('./public'));
+app.use(fileUpload());
+
+app.get('/', (req, res) => {
+  res.send('E-commerce api')
+})
+app.get('/api/v1', (req, res) => {
+  console.log(req.signedCookies);
+  res.send('E-commerce api')
+})
+
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/products', productRouter);
+app.use('/api/v1/reviews', reviewRouter);
+app.use('/api/v1/orders', orderRouter);
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
+
+const port = process.env.PORT || 5000;
+
+
+const start = async () => {
+
+    try {
+        await mongoose.connect(process.env.MONGO_URL)
+       console.log('connected to db')
+    } catch (error) {
+       console.log(error) 
+    }
+}
+app.listen(port, () => {
+    start() 
+  console.log(`app listening on port ${5000}`)
+})
+
